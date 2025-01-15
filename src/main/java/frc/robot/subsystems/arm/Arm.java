41881b342c96f61a68904bda0ec87cac77b6fe5e
rhit-edgingza <edgingza@rose-hulman.edu>
2025-01-15 18:17:33 -0500

    Added io to arm class

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems.arm;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.arm.io.ArmIO;
 
 public class Arm extends SubsystemBase {
-  public Arm() {
+  private ArmIO _io;
 
+  public Arm(ArmIO io) {
+    _io = io;
   }
 
   @Override

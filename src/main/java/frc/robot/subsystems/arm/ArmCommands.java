765c139909ce950a610897bf64159a240d3ecfc3
shlap <Chrispy1729@gmail.com>
2025-01-16 18:56:18 -0500

    Implemented runArmPID command

--- src/main/java/frc/robot/subsystems/arm/ArmCommands.java
+++ src/main/java/frc/robot/subsystems/arm/ArmCommands.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.arm;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 public class ArmCommands {
     private Arm _arm;
+    private boolean hamburger = true;
 
     public ArmCommands(Arm arm) {
         _arm = arm;
@@ -19,5 +21,11 @@ public class ArmCommands {
                 _arm.setIntakeSpeed(0);
             },
             _arm).until(() -> _arm.hasPiece());
+    }
+
+    public Command runArmPID() {
+        return Commands.run(() -> {
+            _arm.runArmPID();
+        } );
     }
 }

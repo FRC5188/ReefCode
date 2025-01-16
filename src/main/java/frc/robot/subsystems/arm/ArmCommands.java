836652eb7b7edc29955f7c67782c849d0333f3d9
Cadence Wynn <hivemind323@gmail.com>
2025-01-16 18:00:56 -0500

    Implemented spit command

--- src/main/java/frc/robot/subsystems/arm/ArmCommands.java
+++ src/main/java/frc/robot/subsystems/arm/ArmCommands.java
@@ -1,9 +1,23 @@
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+
 public class ArmCommands {
     private Arm _arm;
 
     public ArmCommands(Arm arm) {
         _arm = arm;
     }
+    public Command spit() {
+        return new StartEndCommand(
+            () -> {
+                _arm.setIntakeSpeed(.5);
+            },
+        () -> {
+                _arm.setIntakeSpeed(0);
+        },
+        _arm).withTimeout(1);
+    }
 }
+

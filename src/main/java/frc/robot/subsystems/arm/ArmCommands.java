6aa2737d2639e844a917ceb163a1267953b512b6
Cadence Wynn <hivemind323@gmail.com>
2025-01-16 18:26:43 -0500

    implemented setarmPos command

--- src/main/java/frc/robot/subsystems/arm/ArmCommands.java
+++ src/main/java/frc/robot/subsystems/arm/ArmCommands.java
@@ -1,9 +1,20 @@
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.arm.Arm.ArmPosition;
+
 public class ArmCommands {
     private Arm _arm;
 
     public ArmCommands(Arm arm) {
         _arm = arm;
+    }
+    public Command setArmPosition(ArmPosition setpoint) {
+        return new InstantCommand(
+            ()->{
+                _arm.setArmSetpoint(setpoint);
+            },
+            _arm);
     }
 }

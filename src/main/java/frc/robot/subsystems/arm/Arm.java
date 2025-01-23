b25baa23b09c122412c9a5f697e507333fc36550
shlap <Chrispy1729@gmail.com>
2025-01-22 19:46:31 -0500

    Implemented Feedforward

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.units.measure.Resistance;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.arm.io.ArmIO;
@@ -31,6 +32,26 @@ public class Arm extends SubsystemBase {
   private static final double KD = 0;
   private static final double PROFILE_VEL = 0;
   private static final double PROFILE_ACC = 0;
+
+  // In Ohms
+  private static final double RESISTANCE = 1;
+
+  // In inches-pound
+  private static final double STALL_TORQUE = 1;
+
+  // In pounds
+  private static final double ARM_WEIGHT = 1;
+
+  // In inches
+  private static final double DISTANCE_TO_ARM_MOMENT = 1;
+
+  // In Amps
+  private static final double STALL_AMPERAGE = 1;
+
+  private static final double FEEDFORWARD_CONSTANT =
+    ARM_WEIGHT * DISTANCE_TO_ARM_MOMENT * RESISTANCE * STALL_AMPERAGE 
+    /(STALL_TORQUE);
+
 
   public Arm(ArmIO io) {
     _io = io;

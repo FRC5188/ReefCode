088bc0c02f062e326c759bda7e557596652a6bf5
Cadence Wynn <hivemind323@gmail.com>
2025-02-03 18:44:17 -0500

    Implemented scoreGamepieceAtPosition

--- src/main/java/frc/robot/subsystems/elevator/ElevatorCommands.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorCommands.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.elevator;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
 
 public final class ElevatorCommands {
@@ -39,6 +40,10 @@ public final class ElevatorCommands {
             () -> {
             _elevator.setSetpoint(setpoint);
             }, _elevator);
+    }
+
+    public Command waitUntilAtSetpoint() {
+        return new WaitUntilCommand(_elevator::isAtSetpoint);
     }
 }
 
--- src/main/java/frc/robot/subsystems/multisubsystemcommands/MultiSubsystemCommands.java
+++ src/main/java/frc/robot/subsystems/multisubsystemcommands/MultiSubsystemCommands.java
@@ -11,17 +11,17 @@ import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
 
 public class MultiSubsystemCommands {
     public enum OverallPosition {
-        Stow(ElevatorPosition.Stow, ArmPosition.Stow), 
+        Stow(ElevatorPosition.Stow, ArmPosition.Stow),
         Loading(ElevatorPosition.Stow, ArmPosition.Loading),
         L1(ElevatorPosition.L1, ArmPosition.Stow),
         L2(ElevatorPosition.L2, ArmPosition.Stow),
         L3(ElevatorPosition.L3, ArmPosition.Stow),
         L4(ElevatorPosition.L4, ArmPosition.Stow),
         L4_Score(ElevatorPosition.L4, ArmPosition.L4_Score);
-    
-        
+
         ElevatorPosition _elevatorSetpoint;
         ArmPosition _armSetpoint;
+
         OverallPosition(ElevatorPosition elevatorSetpoint, ArmPosition armSetpoint) {
             _elevatorSetpoint = elevatorSetpoint;
             _armSetpoint = armSetpoint;
@@ -40,8 +40,9 @@ public class MultiSubsystemCommands {
     private Arm _arm;
     private ElevatorCommands _elevatorCommands;
     private ArmCommands _armCommands;
-    
-    public MultiSubsystemCommands(Elevator elevator, Arm arm, ElevatorCommands elevatorCommands, ArmCommands armCommands) {
+
+    public MultiSubsystemCommands(Elevator elevator, Arm arm, ElevatorCommands elevatorCommands,
+            ArmCommands armCommands) {
         _elevator = elevator;
         _arm = arm;
         _elevatorCommands = elevatorCommands;
@@ -49,10 +50,36 @@ public class MultiSubsystemCommands {
     }
 
     public Command setOverallSetpoint(OverallPosition setpoint) {
-        return 
-            _elevatorCommands.setElevatorSetpoint(setpoint.getElevatorPosition())
-            .alongWith(_armCommands.setArmPosition(setpoint.getArmPosition()))
-            .unless(() -> !_elevator.canMoveToPos(_elevator.getCurrentPos(), setpoint.getArmPosition()));
+        return _elevatorCommands.setElevatorSetpoint(setpoint.getElevatorPosition())
+                .alongWith(_armCommands.setArmPosition(setpoint.getArmPosition()))
+                .unless(() -> !_elevator.canMoveToPos(_elevator.getCurrentPos(), setpoint.getArmPosition()));
     }
-    
+
+    public Command waitForOverallMechanism() {
+        return _elevatorCommands.waitUntilAtSetpoint()
+                .alongWith(_armCommands.waitUntilAtSetpoint());
+    }
+
+    private Command score(OverallPosition setpoint) {
+        if (setpoint == OverallPosition.L4) {
+            return setOverallSetpoint(OverallPosition.L4_Score)
+                    .andThen(waitForOverallMechanism())
+                    .andThen(_armCommands.spit())
+                    .andThen(setOverallSetpoint(OverallPosition.L4))
+                    .andThen(waitForOverallMechanism());
+        } else {
+            return _armCommands.spit();
+        }
+    }
+
+    public Command scoreGamepieceAtPosition(OverallPosition setpoint) {
+        if (setpoint == OverallPosition.Stow || setpoint == OverallPosition.Loading
+                || setpoint == OverallPosition.L4_Score) {
+            throw new RuntimeException("scoreGamepieceAtPosition cannot run to stow,loading,or L4_score");
+        }
+        return setOverallSetpoint(setpoint)
+                .andThen(waitForOverallMechanism())
+                .andThen(score(setpoint))
+                .andThen(setOverallSetpoint(OverallPosition.Stow));
+    }
 }

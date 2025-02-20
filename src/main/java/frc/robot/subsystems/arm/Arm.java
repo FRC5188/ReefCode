464a97ccf9fcd6d3f0623c7a1d6543cd2066ddae
JCasey7135 <126344360+Rachel-M-Greenlee@users.noreply.github.com>
2025-02-20 18:45:01 -0500

    Implemented hasPiece method

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -18,10 +18,15 @@ public class Arm extends SubsystemBase {
     }
   }
 
+  public enum ArmMode {
+    Coral,
+    Algae;
+  }
+
   private ArmIO _io;
   private ArmIOInputs _inputs;
   private boolean _prevLightSensorVal;
-
+  private ArmMode _armMode;
 
   private ProfiledPIDController _armPidController;
   
@@ -30,12 +35,14 @@ public class Arm extends SubsystemBase {
   private static final double KD = 0;
   private static final double PROFILE_VEL = 0;
   private static final double PROFILE_ACC = 0;
+  private static final double HAS_ALGAE_CURRENT = 2;
 
   public Arm(ArmIO io) {
     _io = io;
     _inputs = new ArmIOInputs();
 
     _armPidController = new ProfiledPIDController(KP, KI, KD, new Constraints(PROFILE_VEL, PROFILE_ACC));
+    _armMode = ArmMode.Coral;
   }
 
   public void setArmSetpoint(ArmPosition setpoint) {
@@ -47,9 +54,15 @@ public class Arm extends SubsystemBase {
   }
   
   public boolean hasPiece() {
-    boolean current = _inputs._lightSensorState;
-    boolean hasPiece = _prevLightSensorVal && !current;
-    _prevLightSensorVal = current;
+    boolean hasPiece;
+    if (_armMode == ArmMode.Coral) {
+      boolean currentState = _inputs._lightSensorState;
+      hasPiece = _prevLightSensorVal && !currentState;
+      _prevLightSensorVal = currentState;
+    } else {
+      hasPiece = _inputs._intakeMotorCurrent >= HAS_ALGAE_CURRENT;
+    }
+    
     return hasPiece;
 }
 

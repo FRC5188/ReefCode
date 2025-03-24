de8b4ea298f5ae905b5809fd646cc2dc519e47a8
area5188 <bradyallen67@gmail.com>
2025-03-24 19:58:40 -0400

    Tuned arm feedforward

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -65,7 +65,7 @@ public class Arm extends SubsystemBase {
 
   public static final double HAS_ALGAE_CURRENT = 40;
 
-  private static final double ARM_FEEDFORWARD_COEFF = 0.4;
+  private static final double ARM_FEEDFORWARD_COEFF = 0.6;
   private static final double ARM_FEEDFORWARD_ANGLE_OFFSET = -22.3;
 
   SysIdRoutine routine = new SysIdRoutine(new Config(),
@@ -176,7 +176,7 @@ public class Arm extends SubsystemBase {
 
   public void runArmPID() {
     double out = _armPidController.calculate(_inputs._armEncoderPositionDegrees)
-        + (ARM_FEEDFORWARD_COEFF * Math.cos(Units.degreesToRadians(_inputs._armEncoderPositionDegrees + ARM_FEEDFORWARD_ANGLE_OFFSET)));
+      + (ARM_FEEDFORWARD_COEFF * Math.cos(Units.degreesToRadians(_inputs._armEncoderPositionDegrees + ARM_FEEDFORWARD_ANGLE_OFFSET)));
     _io.setArmMotorVoltage(Voltage.ofBaseUnits(out, Volt));
   }
 

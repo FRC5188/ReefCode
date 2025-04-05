e053d9adff73e9acc4f235b55b416415f0e57e52
area5188 <bradyallen67@gmail.com>
2025-04-05 12:21:59 -0400

    Readying code for arm fixing

--- src/main/java/frc/robot/subsystems/arm/io/RealArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/io/RealArmIO.java
@@ -22,8 +22,8 @@ import frc.robot.HardwareConstants.DIO;
 public class RealArmIO implements ArmIO {
 
     private static final double POS_AT_90 = 0.422;
-    private static final double POS_AT_0 = 0.168;
-    private static final double ENCODER_CONVERSION = (POS_AT_90 - POS_AT_0) / 90.0;
+    private static final double POS_AT_0 = 0;
+    private static final double ENCODER_CONVERSION = 1;//(POS_AT_90 - POS_AT_0) / 90.0;
     private static final double CORAL_LASERCAN_DISTANCE_MM = 50;
     private static final double ALGAE_LASERCAN_DISTANCE_MM = 20;
 
